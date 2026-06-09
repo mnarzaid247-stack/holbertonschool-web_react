602f29d41fd6c04efed55db89652f9cd6fa3d10c
manar <mnarzaid247@gmail.com>
2026-06-09 11:03:35 +0300

    doing task 2 and 3 amd 4 amd finally 5

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,79 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propertyName: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => `${firstName.charAt(0)}. ${lastName}`;
+
+console.log(printTeacher('John', 'Doe'));
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+
+const student = new StudentClass('John', 'Doe');
+
+console.log(student.displayName());
+console.log(student.workOnHomework());
